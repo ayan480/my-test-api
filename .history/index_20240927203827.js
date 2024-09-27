a2bbcb72750b7da21dcdc372ea06b6129e6159a5
ayan <ayan480@gmail.com>
2024-09-27 21:57:05 +0530

    File attachment save to db and fetch in list modification done.

--- .history/index_20240927203827.js
+++ .history/index_20240927203827.js
@@ -5,15 +5,12 @@ const app = express();
 // Import the auth routes (make sure this is correct)
 const authRoutes = require('./routes/auth');
 
-const noteRoutes = require('./routes/notes');
-
 app.use(express.json());
 
 // Use the imported router as middleware
-app.use('/auth', authRoutes); // authRoutes should be a router function, not an object
-
-app.use('/notes', noteRoutes); 
+app.use('/auth', authRoutes); // `authRoutes` should be a router function, not an object
+app.use('/notes', notesRouter);
 
 app.listen(3000, () => {
-    console.log('Server running on port 3000');
+console.log('Server running on port 3000');
 });
